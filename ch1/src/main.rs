8a1869c920c4ef476efa09797eac709b2d81fdae
Ivan Leung <ivanl8866@gmail.com>
2024-11-29 00:09:01 -0800

    ch1 init commit

---
+++ ch1/src/main.rs
@@ -0,0 +1,24 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+/*
+cargo new {project_name}
+Create a new Rust project directory
+
+cargo init
+Create a new Rust project within current directory
+
+Compile using rustc
+rustc main.rs
+./main
+
+Run the program wihtout executable
+cargo run
+
+Compile using Cargo
+cargo build
+
+Check the program if it can compile wihout executable
+cargo check
+*/
